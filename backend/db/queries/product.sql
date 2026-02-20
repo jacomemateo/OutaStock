76042c95c5dd610a646dc5ff1185053c06ef7222
Mateo Jacome <j300@umbc.edu>
2026-02-20 14:27:54 -0500

    updated to use named parametes

--- backend/db/queries/product.sql
+++ backend/db/queries/product.sql
@@ -1,15 +1,15 @@
 -- name: CreateProduct :exec
 INSERT INTO product_info (name, price)
-VALUES ( $1, $2 );
+VALUES ( @name, @price );
 
 -- name: UpdatePrice :exec
 UPDATE product_info
-SET price = $1
-WHERE product_id = $2;
+SET price = @price
+WHERE product_id = @product_id;
 
 -- name: DeleteProduct :exec
 DELETE FROM product_info
-WHERE product_id = $1;
+WHERE product_id = @product_id;
 
 -- name: GetProducts :many 
 SELECT * FROM product_info;
