b1dde9ddf6285e5bf8b8ff702be7350bd6b4f478
Mateo Jacome <j300@umbc.edu>
2026-03-02 14:48:15 -0500

    added logging to main

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -2,26 +2,41 @@ package main
 
 import (
 	"context"
-	"log"
+	"os"
+	"time"
+    "github.com/rs/zerolog"
+    "github.com/rs/zerolog/log"
 
 	"github.com/jacomemateo/OutaStock/backend/internal/transport"
 	"github.com/jacomemateo/OutaStock/backend/internal/service"
 )
 
 func main() {
+	// Create zerolog logger
+	log.Logger = log.Output(zerolog.ConsoleWriter{
+		Out:        os.Stdout,
+		TimeFormat: time.RFC3339,
+	})
+
+	// Set log level (could come from env)
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
 	connectionString := "postgres://postgres:secret@localhost:5432/vending?sslmode=disable"
 	ctx := context.Background()
 
 	db, err := service.NewDatabase(ctx, connectionString)
 	if err != nil {
-    	log.Fatal("Failed to connect to database: ", err)
+    	log.Fatal().Err(err).Msg("Failed to connect to database")
 	}
-	defer db.Close()
+
+	defer func() {
+		db.Close()
+		log.Info().Msg("Database connection closed")
+	}()
 
 	router := transport.NewRouter(db)
 	if err := router.Start(); err != nil {
-		log.Fatal("Failed to start ECHO server: ", err)
+		log.Fatal().Err(err).Msg("Failed to start ECHO server")
 	}
 
-	log.Println("Database connection closed")
 }
