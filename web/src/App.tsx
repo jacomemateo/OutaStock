e201e560e28dd1cc138e7dbe5a8835281498cf15
Jason Chen <jchen33@umbc.edu>
2026-03-07 18:09:59 -0500

    Added light option but still need to play around with the color scheme

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,8 +3,15 @@ import '@styles/Global.css';
 import { Route, Routes } from 'react-router-dom';
 import LoadingScreen from '@components/LoadingScreen';
 import Template from '@components/Template';
+import { useTheme } from '@contexts/ThemeContext';
+import { useEffect } from 'react';
 
 function App() {
+    const { theme } = useTheme();
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+    }, [theme]);
     return (
         <Routes>
             <Route path="/" element={<LoadingScreen />} />
--- web/src/components/ThemeChoice.tsx
+++ web/src/components/ThemeChoice.tsx
@@ -2,9 +2,11 @@ import '@styles/ThemeChoice.css';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import { useState } from 'react';
+import { useTheme } from '@contexts/ThemeContext';
 
 const ThemeChoice = () => {
-    const [theme, setTheme] = useState<string>('dark');
+    // const [theme, setTheme] = useState<string>('dark');
+    const { theme, setTheme } = useTheme();
     return (
         <div className="theme-choice-container">
             <div className="theme-heading-section">
@@ -14,7 +16,7 @@ const ThemeChoice = () => {
                 <p>Current Theme</p>
                 <div className="theme-option">
                     {theme === 'dark' ? <DarkModeIcon /> : <LightModeIcon />}
-                    <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+                    <select value={theme} onChange={(e) => setTheme(e.target.value as 'dark' | 'light')}>
                         <option value="dark">Dark </option>
                         <option value="light">Light</option>
                     </select>
