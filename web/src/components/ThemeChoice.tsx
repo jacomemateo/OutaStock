eb789428a4966bee943eb522f9d9d2bbf4ef8650
Jason Chen <jchen33@umbc.edu>
2026-03-07 00:41:15 -0500

    Heading tweeks

--- web/src/components/ThemeChoice.tsx
+++ web/src/components/ThemeChoice.tsx
@@ -1,12 +1,15 @@
-
-
+import '@styles/ThemeChoice.css';
+import BedtimeIcon from '@mui/icons-material/Bedtime';
 
 const ThemeChoice = () => {
-    return(
+    return (
         <div className="theme-choice-container">
-            <h1>Theme Choice Page.....</h1>
+            <div className="theme-heading-section">
+                <h1 className="theme-choice-heading">Theme</h1>
+                <p className="theme-choice-subtitle">Customize your appearance</p>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default ThemeChoice;
