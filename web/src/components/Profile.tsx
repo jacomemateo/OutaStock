73618c8b040101671b8f45a148869b17d60d8b9a
Jason Chen <jchen33@umbc.edu>
2026-03-07 14:44:30 -0500

    tweeks to the different tabs in settins

---
+++ web/src/components/Profile.tsx
@@ -0,0 +1,11 @@
+const Profile = () => {
+    return (
+        <div className="profile-container">
+            <div className="profile-heading-section">
+                
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
